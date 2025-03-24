5307dec8e1969be6abbda83987613820988416b5
Badu_Ser <100788930+badu-ser@users.noreply.github.com>
2025-03-25 02:24:56 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php
 // Enable CORS
-header("Access-Control-Allow-Origin: https://your-vercel-domain.vercel.app"); // Replace with your Vercel app URL
+header("Access-Control-Allow-Origin: https://x4-esports-official.vercel.app"); // Replace with your Vercel app URL
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
